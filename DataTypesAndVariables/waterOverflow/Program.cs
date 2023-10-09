995111aa39cf0d566171923b23812001d00d6107
AntoniaDnv <antonia.d.neva@gmail.com>
2023-10-09 20:58:01 +0300

    сссс

---
+++ DataTypesAndVariables/waterOverflow/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace waterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int n = int.Parse(Console.ReadLine());
+            int sum =  0;
+            int ok = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                
+                   
+
+                    if (sum + liters >= 255)
+                    {
+                        sum = sum;
+                        Console.WriteLine($"Insufficient capacity!");
+                      
+
+                    }
+                    if (sum + liters <255)
+                    {
+                        sum += liters;
+                    }
+                   
+                
+               
+               
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
