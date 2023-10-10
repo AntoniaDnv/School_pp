7b1c93ccec1daa09879a305113935acbf85a007a
AntoniaDnv <antonia.d.neva@gmail.com>
2023-10-10 12:19:54 +0300

    hhhh

---
+++ ArryLabPregovor/ArryLabPregovor/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ArryLabPregovor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            string line = "";
+            for(int i = 0; i < n; i++)
+            {
+                int people = int.Parse(Console.ReadLine()); 
+                
+                line = line + people + " ";
+                
+                sum += people;
+            }
+            
+            
+            Console.WriteLine(line);
+            Console.WriteLine(sum);
+        }
+    }
+}
