c533a74812a460548d102de85fb2891dfc81ced0
AntoniaDnv <antonia.d.neva@gmail.com>
2023-10-16 21:23:50 +0300

    gggg

---
+++ ArryLabPregovor/EqualSum/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EqualSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ne razbiram uslovieto
+        }
+    }
+}
---
+++ ArryLabPregovor/MaxSeqOfEqualEl/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MaxSeqOfEqualEl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int count = 0;  
+
+           for(int i  = 0; i < nums.Length; i++) 
+            { 
+                for(int j  = nums[2]; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        count++;
+                        Console.Write($"{j} ");
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
