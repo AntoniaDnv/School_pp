255f167d2709dd65aa1c9fe779a92331b9cce53a
AntoniaDnv <antonia.d.neva@gmail.com>
2023-10-18 14:29:54 +0300

    ggggg

--- HangMan/HangMan/Program.cs
+++ HangMan/HangMan/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HangMan
+﻿using System.Collections.Generic;
+
+namespace HangMan
 {
     internal class Program
     {
@@ -340,7 +342,7 @@
 @" __/══════╩═══"
 
             };
-            static string[] RandomWords()
+            static string[] ReadWordsFromFile()
 
             {
                 string currentDirectory = Directory.GetCurrentDirectory();
@@ -358,16 +360,196 @@
 
             }
 
-            string[] words = RandomWords();
+           
+            string[] words =  ReadWordsFromFile();
             Console.CursorVisible = false ;
-            while(true)
+
+            void DrawCurrentGameState(bool inputIsInvalid, int inccorectGuessCount, string wordToGuess, List<char> playerUsedLetters)
             {
-              //  string word = GetRandomWords(words);
+               
+                Console.Clear();
+                Console.WriteLine(wrongGuess[inccorectGuessCount]);
+                Console.WriteLine($"Guess: {wordToGuess}");
+                Console.WriteLine($"You have too guess {wordToGuess.Length}");
+                Console.WriteLine($"The following letters are used: {String.Join(", ", playerUsedLetters)}");
+                if (inputIsInvalid)
+                {
+                    Console.WriteLine("You should write only a single character!");
+                }
+                Console.Write($"Your symbol: ");
+
+
+            }
+
+            while (true)
+            {
+               string word = GetRandomWord(words);
+                char Underscore = char.Parse("_");
 
                 string wordToGuess = new(Underscore, word.Length);
+                int inccorectGuessCount = 0;
+                List<char> playerUsedLetters = new List<char>();
+
+                
+                DrawCurrentGameState(false, inccorectGuessCount, wordToGuess, playerUsedLetters);
+                Console.ReadLine();
+                
+                PlayGame(word, wordToGuess, inccorectGuessCount, playerUsedLetters);
+                 Console.WriteLine("If you want to play again, press [Enter]. Else, type 'quit':");
+
+                
+               string continuo = Console.ReadLine();
+                 if (continuo == "quit")
+                   {
+                      Console.Clear();
+                      Console.WriteLine("Thamk you for playing! Hangman was closed.");
+                       return;
+                   }
+                 Console.Clear();
+
             }
+
+
+
+
+
+            void PlayGame(string word, string wordToGuess, int inccorectGuessCount,List<char> playerUsedLetters)
+            {
+                while(true)
+                {
+                    string playerInput = Console.ReadLine().ToLower();
+                    if(playerInput.Length != 1)
+                    {
+                        DrawCurrentGameState(false, inccorectGuessCount, wordToGuess, playerUsedLetters);
+                        continue;
+                    }
+                    char playerLetter = char.Parse(playerInput);
+
+                    playerUsedLetters.Add(playerLetter);
+
+                    bool playerLetterIsContained = CheckIfSymbolIsContained(word, playerLetter);
+
+                    if(playerLetterIsContained)
+                    {
+                        wordToGuess = AddLettertoGuessed(word, playerLetter, wordToGuess);
+
+                    }
+                    else
+                    {
+                        inccorectGuessCount++;
+                    }
+                    DrawCurrentGameState(false, inccorectGuessCount, wordToGuess, playerUsedLetters);
+
+                    bool playerWins = CheckIfPlayerWins(wordToGuess, Underscore);
+
+                    if(playerWins) 
+                    {
+                    Console.Clear();
+                        Console.WriteLine(win);
+                        Console.WriteLine($"The word you guessed is [{word}].");
+
+                        break;
+                    }
+                    bool playerLooses = CheckIfPlayerLooses(inccorectGuessCount);
+
+                    if( playerLooses)
+                    {
+                        Console.SetCursorPosition(0, 0);
+                        DrawDeathAnimation(deathAnimation);
+
+                        Console.Clear();
+                        Console.WriteLine(loose);
+                        Console.WriteLine($"The exact word is [{word}]");
+
+                        break;
+                    }
+                }
+                
+                Console.WriteLine("If you want to play again, press [Enter]. Else, type 'quit':");
+                string continuo = Console.ReadLine();
+                if (continuo == "quit")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Thamk you for playing! Hangman was closed.");
+                    return;
+                }
+                Console.Clear();
+            }
+           
+
+
+
+            bool CheckIfSymbolIsContained (string word, char playerLetter)
+            {
+                if (!word.Contains(playerLetter))
+                {
+                    return false;
+                }
+                 
+                return true;
+            }
+
+            string AddLettertoGuessed(string word, char playerLetter, string wordToGuess)
+            {
+                char[] wordToGuessCharArr = wordToGuess.ToCharArray();
+
+                for (int i = 0; i < wordToGuess.Length; i++)
+                {
+                    if(playerLetter == word[i])
+                    {
+                        wordToGuessCharArr[i] = playerLetter;
+                    }
+                }
+                return new String (wordToGuessCharArr);
+            }
+
+            bool CheckIfPlayerWins (string wordToGuess, char Underscore)
+            {
+                if(wordToGuess.Contains(Underscore))
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            bool CheckIfPlayerLooses(int inccorectGuessCount)
+            {
+                const int MaxAllowedIncorrectCharecters = 6;
+                if (inccorectGuessCount == MaxAllowedIncorrectCharecters) 
+                {
+                    return true; 
+                }
+                return false;
+            }
+
+            void DrawDeathAnimation(string[] dethAnimation)
+            {
+                for (int i = 0;i < dethAnimation.Length; i++)
+                {
+                    Console.WriteLine(deathAnimation[i]);
+
+                    Thread.Sleep(100);
+
+                    Console.SetCursorPosition(0, 0);
+
+                }
+            }
+
+
+            
+           
+            string GetRandomWord(string[] words)
+            {
+                Random random = new Random();
+                string word = words[random.Next(words.Length)];
+                return word = word.ToLower();
+            }
+;            
+           
+
         }
-        
-      
+
+
     }
 }
+    
