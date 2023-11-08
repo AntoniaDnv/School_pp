afdacaf326a8da8bae38c23d69094d5404a54efa
AntoniaDnv <antonia.d.neva@gmail.com>
2023-11-08 14:24:06 +0200

    хахахахахахаах

---
+++ MethodsEx/FactorialDivision/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int n = int.Parse(Console.ReadLine());
+          int m = int.Parse(Console.ReadLine());
+
+            Calculations(n, m);
+        }
+        static void Calculations(int n, int m)
+        {
+            int result = 1;
+            for (int i = n; i > 0; i--)
+            {
+                result *= i; 
+            }
+
+            int resultForM = 1;
+            for (int i = m; i > 0; i--)
+            {
+                resultForM *= i;
+            }
+
+            Console.Write($"{(result/resultForM):f2}");
+
+        }
+    }
+}
