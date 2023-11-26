64f7a1d4226ea0e07498c1a8f160aa7572c7ddf0
AntoniaDnv <antonia.d.neva@gmail.com>
2023-11-26 21:44:02 +0200

    dxhhggchg

--- MoreListEx/MoreListEx/Program.cs
+++ MoreListEx/MoreListEx/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace MoreListEx
+﻿
+
+ class Program
 {
-    internal class Program
+    static int CalculateDigitSum(int number)
     {
-        static void Main(string[] args)
+        return number.ToString().Sum(c => c - '0');
+    }
+
+    static string Messaging(List<int> numbers, string text)
+    {
+        var result = new List<char>();
+        int textLength = text.Length;
+
+        foreach (var number in numbers)
         {
-            Console.WriteLine("Hello, World!");
+            int digitSum = CalculateDigitSum(number);
+            int index = digitSum <= textLength ? digitSum % textLength : (digitSum % textLength) - 1;
+
+            char character = text[index];
+            text = text.Remove(index, 1);
+            textLength--;
+
+            result.Add(character);
         }
+
+        return new string(result.ToArray());
+    }
+
+    static void Main()
+    {
+        string numbersInput = Console.ReadLine();
+        List<int> numbers = numbersInput.Split(' ').Select(int.Parse).ToList();
+
+        string message = Console.ReadLine();
+
+        string output = Messaging(numbers, message);
+        Console.WriteLine(output);
     }
 }
---
+++ MoreListEx/RaceCar/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace RaceCar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> time = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            TimeCalculator(time);
+        }
+
+        static void TimeCalculator(List<int> time)
+
+        {
+            double leftTime = 0;
+            double rightTime = 0;
+            for (int i = 0; i < (time.Count/2); i++)
+            {
+                leftTime = leftTime+ time[i];
+                if (time[i] == 0)
+                {
+                    leftTime = leftTime * 0.8;
+                }
+            }
+            for (int i = time.Count; i >= (time.Count / 2)-1; i--)
+            {
+                rightTime = rightTime + time[i-1];
+                if (time[i-1] == 0)
+                {
+                    rightTime = rightTime * 0.8;
+                }
+            }
+           if(rightTime> leftTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftTime:f1}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightTime:f1}");
+            }
+        }
+    }
+}
