832a5229d9c4950d5ac12de4ab630db19f6117ac
AntoniaDnv <antonia.d.neva@gmail.com>
2023-12-20 12:00:08 +0200

    iskam da spq

--- ExamPreperation/BlackFlag/Program.cs
+++ ExamPreperation/BlackFlag/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int days = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            int expectedPlunder = int.Parse(Console.ReadLine());
+
+            double totalPlunder = 0;
+
+            for (int day = 1; day <= days; day++)
+            {
+                totalPlunder += dailyPlunder;
+
+                if (day % 3 == 0)
+                {
+                    totalPlunder += 0.5 * dailyPlunder;
+                }
+
+                if (day % 5 == 0)
+                {
+                    totalPlunder -= 0.3 * totalPlunder;
+                }
+            }
+
+            if (totalPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:F2} plunder gained.");
+            }
+            else
+            {
+                double percentage = (totalPlunder / expectedPlunder) * 100;
+                Console.WriteLine($"Collected only {percentage:f2}% of the plunder.");
+            }
+
         }
     }
 }
---
+++ ExamPreperation/monsterExterminator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace monsterExterminator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] monster = Console.ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+           
+            int[] hero = Console.ReadLine()
+               .Split(",")
+               .Select(int.Parse)
+               .ToArray();
+           
+            int counter = 0;
+            int test = hero[hero.Length - 1];
+            int test2 = monster[0];
+            for (int i = 0; i < monster.Length; i++)
+            {
+                if (monster[i] <= hero[hero.Length-i-1])
+                {
+                    counter++;
+                    hero[hero.Length - i -2] = hero[hero.Length - i - 2] + ( hero [hero.Length - i - 1] - monster[i]);
+
+                    monster[i] = 0;
+
+
+                }
+            }
+            int checker = monster[monster.Length - 1];
+            if (checker == 0)
+            {
+                Console.WriteLine("All Monsters have been beaten!");
+               
+            }
+            else
+            {
+                Console.WriteLine("The solder has been defeated.");
+            }
+
+            Console.WriteLine(counter);
+
+        }
+    }
+}
