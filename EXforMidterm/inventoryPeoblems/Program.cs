d3a45c99f43e4e94f0f9c44422ccf56ede89122b
AntoniaDnv <antonia.d.neva@gmail.com>
2024-01-03 13:25:53 +0200

    aaaa

--- EXforMidterm/inventoryPeoblems/Program.cs
+++ EXforMidterm/inventoryPeoblems/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace inventoryPeoblems
+﻿using System.Runtime.CompilerServices;
+
+namespace inventoryPeoblems
 {
     internal class Program
     {
@@ -7,10 +9,11 @@
             string[] m = Console.ReadLine()
                 .Split(", ");
               
-            Stack<string> materials = new Stack<string>(m);
+            List<string> materials = new List<string>(m);
 
             string[] command = Console.ReadLine()
                 .Split();
+                
             while (command[0] != "Craft!")
             {
                 switch(command[0])
@@ -22,19 +25,51 @@
                         }
                         else
                         {
-                            materials.Push(command[0]);
+                            materials.Add(command[2]);
                         }
                         break;
 
                     case "Drop":
+                        if (materials.Contains(command[2]))
+                        { 
+                           
+
+                            materials.Remove(command[2]);
+                        }
                         break;
 
                     case "Combine":
+                      string[] tokens =   command[3].Split(":");
+                        if (materials.Contains(tokens[0]))
+                        {
+                            int index = 0;
+                            for (int i = 0; i < materials.Count; i++)
+                            {
+                                if (materials[i] == tokens[0])
+                                {
+                                    index = i +1 ;
+                                    i = 100;
+                                }
+
+                            }
+                            materials.Insert(index, tokens[1]);
+                        }
+
+                        break;
+                    case "Renew":
+                        if (materials.Contains(command[2]))
+                        {
+                            string item = command[2];
+                            materials.Remove(item);
+                            materials.Add(item);
+                        }
                         break;
                 }
 
-                command = Console.ReadLine();
+                command = Console.ReadLine()
+                    .Split() ;
             }
+            Console.WriteLine($"{String.Join(" ", materials)}");
         }
     }
 }
