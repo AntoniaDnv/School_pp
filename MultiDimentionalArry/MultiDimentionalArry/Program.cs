00cf6d84cdf7f394a2a3332d0381c87ef5363767
AntoniaDnv <antonia.d.neva@gmail.com>
2024-01-10 09:29:19 +0200

    jjkkjkkjjkjk

---
+++ MultiDimentionalArry/MultiDimentionalArry/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace MultiDimentionalArry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int matrixRow = size[0];
+            int matrixColumn = size[1];
+            int[,] matrix = new int[matrixRow, matrixColumn];
+
+            for(int row = 0; row< matrix.GetLength(0); row++)
+            {
+                int[] elements = Console.ReadLine()
+                     .Split(", ")
+                     .Select(int.Parse)
+                     .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+            Console.WriteLine();
+            for(int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for(int col = 0; col < matrix.GetLength(0); col++)
+                {
+                    Console.Write($"{matrix[row, col]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ MultiDimentionalArry/Zad2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+               .Split(", ")
+               .Select(int.Parse)
+               .ToArray();
+            int matrixRow = size[0];
+            int matrixColumn = size[1];
+            int[,] matrix = new int[matrixRow, matrixColumn];
+            int sum = 0;
+
+            for(int row = 0; row< matrix.GetLength(0); row++)
+            {
+                int[] element = Console.ReadLine()
+                     .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+                for(int col = 0; col< matrix.Length; col++)
+                {
+                    matrix[row, col] = element[col];
+                    sum += matrix[row, col];
+                }
+            }
+            Console.WriteLine(matrixRow);
+            Console.WriteLine(matrixColumn);
+            Console.WriteLine(sum);
+        }
+    }
+}
