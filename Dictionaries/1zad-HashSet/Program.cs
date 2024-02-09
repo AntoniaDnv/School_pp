c310b022ad743e009997ff92187f081bfcd61aef
AntoniaDnv <antonia.d.neva@gmail.com>
2024-02-09 14:15:14 +0200

    ddd

---
+++ Dictionaries/1zad-HashSet/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1zad_HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+
+            HashSet<string> set = new HashSet<string>();
+
+            while (input[0] != "END")
+            {
+                switch (input[0])
+                {
+                    case "IN":
+                        set.Add(input[1]);
+                        break;
+                    case "OUT":
+                        set.Remove(input[1]);
+                        break;
+                }
+                input = Console.ReadLine().Split(", ");
+
+            }
+            if(set.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach(var carNum in set)
+                {
+                    Console.WriteLine(carNum);
+                }
+            }
+        }
+    }
+}
---
+++ Dictionaries/zad2-HashSet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace zad2_HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for(int i = 0; i < n; i++)
+            {
+                string a = Console.ReadLine();
+                set.Add(a);
+            }
+            foreach(string a in set)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
