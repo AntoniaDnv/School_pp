6787c4422cbe77b29f0e22e612f0adbf884d032b
AntoniaDnv <antonia.d.neva@gmail.com>
2024-03-13 14:12:45 +0200

    StringNText lab

---
+++ stringText/textFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace textFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bandWords = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+            foreach (var bandWord in bandWords)
+            {
+                if (text.Contains(bandWord))
+                {
+                    text = text.Replace(bandWord, new string('*', bandWord.Length));
+                }
+
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
