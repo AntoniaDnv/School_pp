9c5634f4f6963f00ce1b0f834a67dc2ff7d8c09a
AntoniaDnv <antonia.d.neva@gmail.com>
2024-04-17 14:18:32 +0300

    oop exercise

---
+++ oop/oop/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            
+            int heigth = int.Parse(Console.ReadLine());
+            string color = Console.ReadLine();
+
+             Rectangle firstRect = new Rectangle(width, heigth, color);
+            Console.WriteLine($"Rect ({firstRect.Width}, {firstRect.Height}, {firstRect.Color}) has area {firstRect.CalcArea()}.");
+        }
+    }
+}
