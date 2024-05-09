db03652a80943b786bdba0c1a7e39c01a50666fb
AntoniaDnv <antonia.d.neva@gmail.com>
2024-05-09 20:29:54 +0300

    update

--- TeamWorkProject/TeamWorkProject/Program.cs
+++ TeamWorkProject/TeamWorkProject/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace TeamWorkProject
+﻿using System.IO.Pipes;
+
+namespace TeamWorkProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           int n = int.Parse(Console.ReadLine());
+          
+          var teams = new Dictionary<string, List<string>>();
+            for (int i = 0; i < n; i = 0)
+            {
+                string[] input = Console.ReadLine().Split("-");
+                string user = input[0];
+                string teamName = input[1];
+                if (teamName.Contains(">") && teams.ContainsKey(teamName))
+                {
+                    teamName.Split(">");
+                    if (!teams.ContainsValue(teams[user]))
+                    {
+                        teams[teamName].Add(user);
+                    }
+                }
+                else
+                {
+                    i++;
+                    teams.Add(teamName, );
+                }
+            }
+
         }
+    }
+
+    public class Projects
+    {
+        public string User { get; set; }
+        public string TeamName { get; set; }
+
+        public Projects(string user, string teamName) 
+        { 
+            User = user;
+            TeamName = teamName;
+        }
+
+
     }
 }
