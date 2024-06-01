745accceb396e5a50f03ece973ede5add809ea31
AntoniaDnv <antonia.d.neva@gmail.com>
2024-06-01 16:24:48 +0300

    exeption handeling

--- WordCount/WordCount/Program.cs
+++ WordCount/WordCount/Program.cs
@@ -6,18 +6,24 @@
         {
            string wordsPath = @"..\..\..\Files\words.txt";
            string textPath = @"..\..\..\Files\text.txt";
+           string outputPath = @"..\..\..\Files\output.txt";
 
             string[] words = {wordsPath};
-           
-
+          WordCount wordCount = new WordCount();
+        
         }
     }
     public class WordCount
     {
         public void CalculateWordCounts(string wordsPath, string textPath,
-        string outputFilePath)
+        string outputPath)
         {
-
+            using(var reader = new StreamReader(wordsPath))
+            {
+                int count = 0;
+                for (int i = 0; i <)
+            }
+           
         }
     }
 }
---
+++ exeption handeling/sum of intigers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Linq;
+
+namespace sum_of_intigers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int sum = 0;
+            int currentNum = 0;
+            try
+            {
+                int[] nums = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    currentNum = nums[i];
+                    sum += currentNum;
+                    Console.WriteLine($"Element '{currentNum}' processed - current sum: {sum}");
+                }
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine($"The element {currentNum} is out of range!");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine($"The element {currentNum} is in wrong format!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
