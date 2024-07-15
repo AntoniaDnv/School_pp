20eb20a545a352cdc68aec1a19da4522f4b6f719
AntoniaDnv <antonia.d.neva@gmail.com>
2024-07-15 13:04:04 +0300

    exercises and  summer practice

---
+++ Lasagna/Lasagna/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Timers;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lasagna
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lasagna = new Lasagna();
+            int minutesInOven = lasagna.ExpectedMinutesInOven();
+            int remainingMinutesInOven = lasagna.RemainingMinutesInOven(30);
+            int leyarsTime = lasagna.PreparationTimeInMinutes(2);
+            int totalTime = lasagna.ElapsedTimeInMinutes(3, 20);
+            Console.WriteLine(totalTime);
+
+        }
+            class Lasagna
+            {
+            public int Test() => 40;
+                public int ExpectedMinutesInOven()
+                {
+                    return 40;
+                }
+                public int RemainingMinutesInOven(int minutesInOven)
+                {
+                    int totalMinutes = ExpectedMinutesInOven();
+                    return totalMinutes - minutesInOven;
+                }
+                public int PreparationTimeInMinutes(int leyars)
+                {
+                    return leyars * 2;
+                }
+                public int ElapsedTimeInMinutes(int leyarsTime, int minutesSpentInOven)
+                {
+                    return leyarsTime + minutesSpentInOven;
+                }
+            public static string Message(string logLine)
+            {
+                string[] fuckU = logLine.Split(": ", StringSplitOptions.RemoveEmptyEntries);
+                char[] charsToTrim = { '[', ']' };
+               
+                return fuckU[0].ToLower().Trim();
+            }
+        }
+    }
+} 
+
+
+   
