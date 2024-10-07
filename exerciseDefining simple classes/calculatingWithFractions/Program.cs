3e075d0384a4b3fbf1614a8b31fa2c917b690b24
Antonia Dineva <antonia.d.neva@gmail.com>
2024-10-07 12:09:21 +0300

    update

---
+++ exerciseDefining simple classes/calculatingWithFractions/Program.cs
@@ -0,0 +1,19 @@
+﻿using Fractions;
+using System.Security.Cryptography.X509Certificates;
+namespace calculatingWithFractions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          var input =  Console.ReadLine().Split().ToArray();
+
+            int[] firstFractionInput = input[0].Split("/").Select(int.Parse).ToArray();
+            int[] secondFractionInput = input[2].Split("/").Select(int.Parse).ToArray();
+            string sign = input[1];
+
+            Fraction fractionOne = new Fraction(firstFractionInput[0], firstFractionInput[1]);
+            Fraction fractionTwo = new Fraction(secondFractionInput[0], secondFractionInput[1]);
+        }
+    }
+}
