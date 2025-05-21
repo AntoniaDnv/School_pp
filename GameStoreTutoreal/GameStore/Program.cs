b27ac4c79d1877b2964eefb252aec70adbdd6bb9
Antonia <119491435+AntoniaDnv@users.noreply.github.com>
2025-05-21 17:34:02 +0300

    .net tutoreal

--- GameStoreTutoreal/GameStore/Program.cs
+++ GameStoreTutoreal/GameStore/Program.cs
@@ -1,69 +1,9 @@
 using System.Security.Cryptography;
-using GameStore.Dtos;
+using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-const string GetGameEndpointName = "GetGame";
-
-List<GameDto> games = [
-    new(
-        1,
-        "Street Fighter two",
-        "Fighting",
-        19.99M,
-        new DateOnly(1992, 7, 15)),
-    new(
-            2,
-            "Final Dancing",
-            "Roleplay",
-            59.99M,
-            new DateOnly(2010, 9, 30))
-];
-//GET /games
-app.MapGet("games", () => games);
-
-//GET /games/1
-app.MapGet("games/{id}", (int id) =>
-{
-    GameDto? game = games.Find(game => game.Id == id);
-    return game is null? Results.NotFound(): Results.Ok(game);
-})
-.WithName(GetGameEndpointName);
-
-//POST /games
-app.MapPost("games", (CreateGameDto newGame) =>
-{
-    GameDto game = new GameDto(
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate);
-    games.Add(game);
-
-    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
-});
-
-//PUT /games
-app.MapPut("games/{id}", (int id, UpdateGameDto updateGame) =>
-{
-    var index = games.FindIndex(game => game.Id == id);
-    games[index] = new GameDto(
-        id,
-        updateGame.Name,
-        updateGame.Genre,
-        updateGame.Price,
-        updateGame.ReleaseDate
-    );
-
-    return Results.NoContent();
-});
-
-app.MapDelete("games/{id}", (int id) =>
-{
-    games.RemoveAll(game => game.Id == id);
-    return Results.NoContent();
-});
+app.MapGamesEndpoints();
 
 app.Run();
