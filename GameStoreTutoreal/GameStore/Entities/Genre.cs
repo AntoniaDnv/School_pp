b915e6dab4349aca6847d087cadb00e8c399851d
Antonia <119491435+AntoniaDnv@users.noreply.github.com>
2025-05-23 13:37:22 +0300

    asp.net core course update
    dbContext

--- GameStoreTutoreal/GameStore/Entities/Genre.cs
+++ GameStoreTutoreal/GameStore/Entities/Genre.cs
@@ -5,6 +5,10 @@ namespace GameStore.Entities;
 public class Genre
 {
     public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public decimal Price { get; set; }
    
-   public required string Name { get; set; } 
+   public DateOnly ReleaseDate { get; set; } 
 }
--- GameStoreTutoreal/GameStore/Program.cs
+++ GameStoreTutoreal/GameStore/Program.cs
@@ -1,7 +1,12 @@
 using System.Security.Cryptography;
+using GameStore.Data;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
