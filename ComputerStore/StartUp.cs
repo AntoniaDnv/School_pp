6922fa67fa198b7a7312475f7309c3088bfa97e8
Antonia <119491435+AntoniaDnv@users.noreply.github.com>
2025-06-10 13:27:18 +0300

    Final exam 10 grade c#

---
+++ ComputerStore/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using ComputerStore.InterfacesAndClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComputerStore
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
